a364e9c13d3ab0a56e055b6f7fb1e558d1aad350
Nahid-subi <scientisthassannahid@gmail.com>
2023-04-11 21:31:38 +0600

    Statistics file done graph added

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
-
+import { Chart as ChartJS, Legend, LineElement, Title, Tooltip, CategoryScale, LinearScale, PointElement, Filler } from 'chart.js';
+import React, { useState } from 'react';
+import { Line } from 'react-chartjs-2';
+ChartJS.register(
+    Legend, LineElement, Title, Tooltip, CategoryScale, LinearScale, PointElement, Filler
+)
 const Statistics = () => {
+    const [data, setData] = useState({
+        labels: ["Assignment 1", "Assignment 2", "Assignment 3", "Assignment 4", "Assignment 5", "Assignment 6", "Assignment 7", "Incoming"],
+        datasets: [
+            {
+                label: 'Assignment',
+                data: [60, 60, 60, 60, 59, 60, 60, 0],
+                tension: 0.4,
+                borderColor: '#8c00ff',
+                backgroundColor: 'rgba(119, 0, 255, 0.5)',
+                fill: true,
+            }
+        ]
+    })
     return (
-        <div>
-            <h1>statistics</h1>
+        <div className=''>
+            <section className='bg-gray-100 h-48 flex justify-center items-center'>
+                <h1 className='font-bold text-2xl'>Statistics</h1>
+            </section>
+            <div className='lg:max-w-screen-xl mx-auto my-10'>
+                <Line data={data}>Hello</Line>
+            </div>
         </div>
     );
 };
