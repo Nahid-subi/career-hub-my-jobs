7710c8b4b6415f65a8daf1c05a68b58e8b081321
Nahid-subi <scientisthassannahid@gmail.com>
2023-04-11 22:40:41 +0600

    remote job and onsite button working done

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -1,33 +1,54 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { getStoredCart } from '../utils/fakedDB';
 import { ProductContext } from '../App';
 import AppliedCard from './Card/AppliedCard';
 
 const AppliedJobs = () => {
-    const products = useContext(ProductContext)
-    const savedCart = getStoredCart()
-    let cart = []
-    for(const id in savedCart){
-        const foundProduct =  products.find(product => product.id == id)
-        if(foundProduct){
-            cart.push(foundProduct)
-        }
+  const products = useContext(ProductContext);
+  const savedCart = getStoredCart();
+  const [showRemote, setShowRemote] = useState(null);
+
+  let cart = [];
+  for (const id in savedCart) {
+    const foundProduct = products.find((product) => product.id == id);
+    if (foundProduct) {
+      cart.push(foundProduct);
     }
-    return (
-        <div>
-             <section className='bg-gray-100 h-48 flex justify-center items-center'>
-                <h1 className='font-bold text-2xl'>Applied Jobs</h1>
-            </section>
-            <div className='lg:max-w-screen-xl mx-auto my-48'>
-                {
-                    cart.map(data => <AppliedCard
-                    key={data.id}
-                    data={data}
-                    ></AppliedCard>)
-                }
-            </div>
+  }
+
+  const remoteCart = cart.filter((data) => data.remote === true);
+  const onsiteCart = cart.filter((data) => data.remote === false);
+
+  return (
+    <div>
+      <section className='bg-gray-100 h-48 flex justify-center items-center'>
+        <h1 className='font-bold text-2xl'>Applied Jobs</h1>
+      </section>
+      <div className='lg:max-w-screen-xl mx-auto my-48'>
+        <div className='flex justify-end'>
+          <button className='btn' onClick={() => setShowRemote(false)}>
+            Onsite Job
+          </button>
+          <button className='btn ms-3' onClick={() => setShowRemote(true)}>
+            Remote Job
+          </button>
         </div>
-    );
+        <div>
+          {showRemote === null
+            ? cart.map((data) => (
+                <AppliedCard key={data.id} data={data}></AppliedCard>
+              ))
+            : showRemote
+            ? remoteCart.map((data) => (
+                <AppliedCard key={data.id} data={data}></AppliedCard>
+              ))
+            : onsiteCart.map((data) => (
+                <AppliedCard key={data.id} data={data}></AppliedCard>
+              ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AppliedJobs;
